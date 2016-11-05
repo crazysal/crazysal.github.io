6895713af34d862fffe24df1f264b04c3ca27c8e
crazysal <crazysal.14@gmail.com>
2016-11-05 22:27:24 +0530

    Url in Js, local lib, prog-intro1

---
+++ js/sidebar.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("active");
+  });
+  /*Urls*/
+  //gyaan
+  $("a#koepke").attr("href", "https://crazysal.github.io/content/gyaan/breathe.html")
+  $("a#urquico").attr("href", "https://crazysal.github.io/content/gyaan/girl-who-writes.html")
+    //tech
+  $("a#win32").attr("href", "https://crazysal.github.io/content/tech/win32_sality.html")
+  $("a#prog-1").attr("href", "https://crazysal.github.io/content/tech/prog-intro1.html")
+    //Social
+  $("a.twitter").attr("href", "https://twitter.com/crazysal").attr("title", "Follow my Tweets")
+  $("a.facebook").attr("href", "https://www.facebook.com/Crazy.sal14").attr("title", "Facebook")
+  $("a.instagram").attr("href", "https://instagram.com/mauvejedi/").attr("title", "Insta")
+//<li><a class="googleplus" href="#" title="Google+">Google+</a></li> 
+//<li><a class="dribbble" href="#" title="Dribbble">Dribbble</a></li> 
+//<li><a class="flickr" href="#" title="Flickr">Flickr</a></li> 
+});
