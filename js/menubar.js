61d97b07ef1852340452d303afa054f4eeb07878
crazysal <crazysal.14@gmail.com>
2016-11-06 22:09:23 +0530

    Added menu, contact, abot page

---
+++ js/menubar.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  /*Urls*/
+  //topnav
+  $("a#home").attr("href", "https://crazysal.github.io");
+  $("a#about").attr("href", "https://crazysal.github.io/about.html")
+  $("a#resume").attr("href", "https://crazysal.github.io/resume.pdf")
+  $("a#contact").attr("href", "https://crazysal.github.io/contact.html")
+});
